926628193c340cc233fb0af8d59756b372f116ae
LimeTree0 <bum9516@gmail.com>
2023-01-04 18:51:21 +0900

    이분 검색 수정

--- Algorithm/binary_search.cpp
+++ Algorithm/binary_search.cpp
@@ -1,38 +1,64 @@
-//#include<iostream>
-//#define N 100
-//
-//int bisearch(int n, int numArray[], int key) {
-//	int low, high, middle, location, value;
-//	low = 0, high = n - 1, location = -1;
-//
-//	while (low <= high && location == -1) {
-//		middle = (low + high) / 2;
-//		value = numArray[middle];
-//		if (value == key)
-//			location = middle;
-//		else if (value > key)
-//			high = middle - 1;
-//		else
-//			low = middle + 1;
-//	}
-//
-//	return location;
-//}
-//
-//
-//int main() {
-//
-//	int numArray[N], key;
-//	key = 28;
-//	for (int i = 1; i < N + 1; i++)
-//		numArray[i - 1] = i*2;
-//
-//	int location = bisearch(N, numArray, key);
-//
-//	if (location == -1) {
-//		printf_s("'%d' does not exist\n", key);
-//		return 0;
-//	}
-//
-//	printf_s("location of '%d' in the array : %d", key, location);
-//}
+#include<iostream>
+#define N 10	// 배열의 크기
+
+int x = 5;
+int S[N] = { 1,2,3,4,5,6,7,8,9,10 };
+
+// 이분 탐색 함수(반복문 활용)
+/*
+int bisearch(int n, int numArray[], int key) {
+	int low, high, middle, location, value;
+	low = 0, high = n - 1, location = -1;
+
+	while (low <= high && location == -1) {
+		middle = (low + high) / 2;
+		value = numArray[middle];
+		if (value == key)
+			location = middle;
+		else if (value > key)
+			high = middle - 1;
+		else
+			low = middle + 1;
+	}
+
+	return location;
+}
+*/
+
+// 재귀 함수를 이용한 이분탐색
+int location(int low, int high)
+{
+	int mid;
+
+	if (low > high) return -1;	// 배열에 존재하지 않는 경우
+	else {
+		mid = (low + high) / 2;
+		if (x == S[mid]) return mid;
+		else if (x < S[mid]) location(low, mid - 1);
+		else return location(mid + 1, high);
+	}
+}
+
+// 반복문을 이용한 이분탐색
+int location2(int low, int high)
+{
+	int mid;
+
+	while (low <= high) {	// 배열에 존재하지 않으면 종료
+		mid = (high + low) / 2;
+		if (S[mid] == x) return mid;
+		else if (x < S[mid]) high = mid - 1;
+		else low = mid + 1;
+	}
+
+	return -1;
+}
+
+
+int main() {
+
+	int key = location(0, N - 1);
+	printf("재귀 함수 이분검색 : %d\n", key);
+	key = location2(0, N - 1);
+	printf("반복문 이분 검색 : %d\n", key);
+}
--- Algorithm/sequential_search.cpp
+++ Algorithm/sequential_search.cpp
@@ -1,35 +1,35 @@
-#include<iostream>
-
-int seqsearch(int size, int numArray[], int key) {	//size: 배열의 크기 numArray: 배열 key: 검색하고 싶은 요소
-
-	int location = 0;	// location 0으로 초기화
-
-	// 배열에서 키를 찾거나 배열 끝에 도달하면 반복문 종료
-	while (location < size && key != numArray[location])
-		location++;
-
-	// 배열의 끝에 도달한 경우 -1 반환
-	if (location == size)
-		return -1;
-
-	// 아닌 경우(키를 찾음) location 반환
-	return location;
-}
-
-
-int main() {
-	
-	int  key;
-	int numArray[] = {1,9,4,2,3,4,7,8};
-	int sizeOfNumArray = sizeof(numArray) / 4;
-	key = 4;
-
-	int location = seqsearch(sizeOfNumArray, numArray, key);
-
-	if (location == -1) {
-		printf_s("'%d' does not exist\n", key);
-		return 0;
-	}
-
-	printf_s("key : %d location : %d", key, location);
-}
+//#include<iostream>
+//
+//int seqsearch(int size, int numArray[], int key) {	//size: 배열의 크기 numArray: 배열 key: 검색하고 싶은 요소
+//
+//	int location = 0;	// location 0으로 초기화
+//
+//	// 배열에서 키를 찾거나 배열 끝에 도달하면 반복문 종료
+//	while (location < size && key != numArray[location])
+//		location++;
+//
+//	// 배열의 끝에 도달한 경우 -1 반환
+//	if (location == size)
+//		return -1;
+//
+//	// 아닌 경우(키를 찾음) location 반환
+//	return location;
+//}
+//
+//
+//int main() {
+//	
+//	int  key;
+//	int numArray[] = {1,9,4,2,3,4,7,8};
+//	int sizeOfNumArray = sizeof(numArray) / 4;
+//	key = 4;
+//
+//	int location = seqsearch(sizeOfNumArray, numArray, key);
+//
+//	if (location == -1) {
+//		printf_s("'%d' does not exist\n", key);
+//		return 0;
+//	}
+//
+//	printf_s("key : %d location : %d", key, location);
+//}
